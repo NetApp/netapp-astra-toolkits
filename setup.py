b478109b084ad387753d085219a8a8d3d399a4e6
Paetzel, Josh <Josh.Paetzel@netapp.com>
2021-10-04 17:39:36 -0500

    Get ready to push 2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="netapp-astra-toolkits",
-    version="1.3",
+    version="2.0",
     scripts=["toolkit.py"],
     py_modules=["astraSDK"],
     data_files=[("config", ["config.yaml"])],
